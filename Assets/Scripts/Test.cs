c92b1e79c1073ec8070f92cee7ab59ad9492cd93
Burhan Uddin <whoisburhan@gmail.com>
2023-01-12 15:27:56 +0600

    push

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("A");
     }
 }
